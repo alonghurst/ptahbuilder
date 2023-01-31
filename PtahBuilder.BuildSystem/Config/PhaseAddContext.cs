6c5b72468f78f0fe8cfd7e25d08376481728bc07
Andy Longhurst <andy@longhurst.net>
2023-01-31 09:51:07 +0000

    did phases

---
+++ PtahBuilder.BuildSystem/Config/PhaseAddContext.cs
@@ -0,0 +1,19 @@
+﻿namespace PtahBuilder.BuildSystem.Config;
+
+public class PhaseAddContext
+{
+    public Dictionary<Type, PipelineConfig> EntityPipelines { get; } = new ();
+
+    public PhaseAddContext AddPipeline<T>(Action<PipelineConfig<T>> configure, string? name = null)
+    {
+        name = string.IsNullOrWhiteSpace(name) ? $"{typeof(T).Name}_Pipeline" : name;
+
+        var pipeline = new PipelineConfig<T>(name);
+
+        configure(pipeline); 
+
+        EntityPipelines.Add(typeof(T), pipeline);
+
+        return this;
+    }
+}
