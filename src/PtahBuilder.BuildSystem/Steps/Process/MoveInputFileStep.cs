7df98a189b6da2fe1ca0ac8e123b541f212e1b0d
Andy Longhurst <andy@longhurst.net>
2023-02-13 15:56:50 +0000

    sorted namespaces

--- src/PtahBuilder.BuildSystem/Steps/Process/MoveInputFileStep.cs
+++ src/PtahBuilder.BuildSystem/Steps/Process/MoveInputFileStep.cs
@@ -1,57 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PtahBuilder.BuildSystem.Config;
+﻿using PtahBuilder.BuildSystem.Config;
 using PtahBuilder.BuildSystem.Entities;
 using PtahBuilder.BuildSystem.Execution.Abstractions;
 using PtahBuilder.BuildSystem.Services.Files;
 
-namespace PtahBuilder.BuildSystem.Steps.Process
+namespace PtahBuilder.BuildSystem.Steps.Process;
+
+public class MoveInputFileStep<T>:IStep<T>
 {
-    public class MoveInputFileStep<T>:IStep<T>
+    private readonly string _propertyName;
+    private readonly IInputFileService _inputFileService;
+
+    public MoveInputFileStep( IInputFileService inputFileService, string propertyName)
     {
-        private readonly string _propertyName;
-        private readonly IInputFileService _inputFileService;
+        _propertyName = propertyName;
+        _inputFileService = inputFileService;
+    }
 
-        public MoveInputFileStep( IInputFileService inputFileService, string propertyName)
+    public async Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        var property = typeof(T).GetProperty(_propertyName) ?? throw new InvalidOperationException($"Unable to find property named {_propertyName}");
+
+        foreach (var entity in entities)
         {
-            _propertyName = propertyName;
-            _inputFileService = inputFileService;
-        }
-
-        public async Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
-        {
-            var property = typeof(T).GetProperty(_propertyName) ?? throw new InvalidOperationException($"Unable to find property named {_propertyName}");
-
-            foreach (var entity in entities)
+            if (entity.Metadata.TryGetValue(MetadataKeys.SourceFile, out var value) && value is string filename)
             {
-                if (entity.Metadata.TryGetValue(MetadataKeys.SourceFile, out var value) && value is string filename)
+                var desiredCategory = property.GetValue(entity)?.ToString();
+
+                if (!string.IsNullOrWhiteSpace(desiredCategory))
                 {
-                    var desiredCategory = property.GetValue(entity)?.ToString();
+                    var currentDirectory = Path.GetFileName(Path.GetDirectoryName(filename) ?? string.Empty);
 
-                    if (!string.IsNullOrWhiteSpace(desiredCategory))
+                    if (currentDirectory != desiredCategory)
                     {
-                        var currentDirectory = Path.GetFileName(Path.GetDirectoryName(filename) ?? string.Empty);
-
-                        if (currentDirectory != desiredCategory)
-                        {
-                            var desiredDirectory = Path.Combine(_inputFileService.GetInputDirectoryForEntityType<T>(), desiredCategory);
+                        var desiredDirectory = Path.Combine(_inputFileService.GetInputDirectoryForEntityType<T>(), desiredCategory);
                             
-                            if (!Directory.Exists(desiredDirectory))
-                            {
-                                Directory.CreateDirectory(desiredDirectory);
-                            }
-
-                            var desiredFilename = Path.Combine(desiredDirectory, Path.GetFileName(filename));
-
-                            var contents = await File.ReadAllTextAsync(filename);
-
-                            await File.WriteAllTextAsync(desiredFilename, contents);
-
-                            File.Delete(filename);
+                        if (!Directory.Exists(desiredDirectory))
+                        {
+                            Directory.CreateDirectory(desiredDirectory);
                         }
+
+                        var desiredFilename = Path.Combine(desiredDirectory, Path.GetFileName(filename));
+
+                        var contents = await File.ReadAllTextAsync(filename);
+
+                        await File.WriteAllTextAsync(desiredFilename, contents);
+
+                        File.Delete(filename);
                     }
                 }
             }
