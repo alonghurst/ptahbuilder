64b241803e431eac3d26ecf4bb81aa7fb2f6c06d
Andy Longhurst <andy@longhurst.net>
2023-02-23 15:07:17 +0000

    added validator

--- src/PtahBuilder.BuildSystem/Steps/Process/ValidateEntityReferenceStep.cs
+++ src/PtahBuilder.BuildSystem/Steps/Process/ValidateEntityReferenceStep.cs
@@ -8,7 +8,8 @@ public class ValidateEntityReferenceStep<TFrom, TTo> : IStep<TFrom>
 {
     private readonly IEntityProvider<TTo> _referencing;
     private readonly ILogger _logger;
-    private readonly string _propertyName;
+    private readonly string? _propertyName;
+    private readonly Func<TFrom, object?>? _accessor;
 
     public ValidateEntityReferenceStep(IEntityProvider<TTo> referencing, ILogger logger, string propertyName)
     {
@@ -17,13 +18,23 @@ public class ValidateEntityReferenceStep<TFrom, TTo> : IStep<TFrom>
         _propertyName = propertyName;
     }
 
+    public ValidateEntityReferenceStep(IEntityProvider<TTo> referencing, ILogger logger, Func<TFrom, object?> accessor)
+    {
+        _referencing = referencing;
+        _logger = logger;
+        _accessor = accessor;
+    }
+
     public Task Execute(IPipelineContext<TFrom> context, IReadOnlyCollection<Entity<TFrom>> entities)
     {
-        var property = typeof(TFrom).GetProperty(_propertyName) ?? throw new InvalidOperationException($"Unable to find a property named {_propertyName}");
+        var getter = _accessor ?? 
+                     CreatePropertyGetters() ?? 
+                     throw new InvalidOperationException($"Unable to create a getter for validating {typeof(TFrom).Name} to {typeof(TTo).Name}");
 
+       
         foreach (var entity in entities)
         {
-            var reference = property.GetValue(entity.Value);
+            var reference = getter(entity.Value);
 
             if (reference is IEnumerable<string> strings)
             {
@@ -43,6 +54,18 @@ public class ValidateEntityReferenceStep<TFrom, TTo> : IStep<TFrom>
         }
 
         return Task.CompletedTask;
+    }
+
+    private Func<TFrom, object?>? CreatePropertyGetters()
+    {
+        if (string.IsNullOrWhiteSpace(_propertyName))
+        {
+            return null;
+        }
+
+        var property = typeof(TFrom).GetProperty(_propertyName) ?? throw new InvalidOperationException($"Unable to find a property named {_propertyName}");
+
+        return x => property.GetValue(x);
     }
 
     private void Validate(IPipelineContext<TFrom> context, Entity<TFrom> entity, string id)
