c9df2419e4b76c74aa6d46f2b003fb15187d387d
Andy Longhurst <andy@longhurst.net>
2023-04-13 16:28:05 +0100

    added info from

---
+++ src/PtahBuilder.Util/Extensions/LoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using PtahBuilder.Util.Services.Logging;
+
+namespace PtahBuilder.Util.Extensions;
+
+public static class LoggerExtensions
+{
+    public static void InfoFrom(this ILogger logger, object context, string message)
+    {
+        var desc = context.GetType().GetTypeName();
+
+        logger.Info($"[{desc}] {message}");
+    }
+}
