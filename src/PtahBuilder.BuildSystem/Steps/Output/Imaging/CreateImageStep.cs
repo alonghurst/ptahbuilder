cf0128baca65ff69ef818b69490fd00cd79555a6
Andy Longhurst <andy@longhurst.net>
2023-04-28 16:44:11 +0100

    image creation steps

--- src/PtahBuilder.BuildSystem/Steps/Output/Imaging/CreateImageStep.cs
+++ src/PtahBuilder.BuildSystem/Steps/Output/Imaging/CreateImageStep.cs
@@ -1,49 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PtahBuilder.BuildSystem.Config;
 using PtahBuilder.BuildSystem.Entities;
 using PtahBuilder.BuildSystem.Execution.Abstractions;
-using Spectre.Console.Rendering;
 
 #pragma warning disable CA1416 // Validate platform compatibility
-namespace PtahBuilder.BuildSystem.Steps.Output.Imaging
+namespace PtahBuilder.BuildSystem.Steps.Output.Imaging;
+
+public record ImageOutputConfig(string Filename, int Width, int Height, ImageFormat? ImageFormat = null);
+
+public abstract class CreateImageStep<T> : IStep<T>
 {
-    public record ImageOutputConfig(string Filename, int Width, int Height, ImageFormat? ImageFormat = null);
+    private readonly IFilesConfig _filesConfig;
 
-    public abstract class CreateImageStep<T> : IStep<T>
+    protected CreateImageStep(IFilesConfig filesConfig)
     {
-        private readonly IFilesConfig _filesConfig;
-
-        protected CreateImageStep(IFilesConfig filesConfig)
-        {
-            _filesConfig = filesConfig;
-        }
-
-        public async Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
-        {
-            var config = CreateConfig(context, entities);
-
-            using (var image = new Bitmap(config.Width, config.Height))
-            {
-                using (var graphics = Graphics.FromImage(image))
-                {
-                    await Render(context, entities, graphics);
-                }
-
-                var path = Path.Combine(_filesConfig.OutputDirectory, config.Filename);
-
-                image.Save(path, config.ImageFormat ?? ImageFormat.Png);
-            }
-        }
-
-        protected abstract Task Render(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities, Graphics graphics);
-
-        protected abstract ImageOutputConfig CreateConfig(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities);
+        _filesConfig = filesConfig;
     }
+
+    public async Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        var config = CreateConfig(context, entities);
+
+        using (var image = new Bitmap(config.Width, config.Height))
+        {
+            using (var graphics = Graphics.FromImage(image))
+            {
+                await Render(context, entities, graphics);
+            }
+
+            var path = Path.Combine(_filesConfig.OutputDirectory, config.Filename);
+
+            image.Save(path, config.ImageFormat ?? ImageFormat.Png);
+        }
+    }
+
+    protected abstract Task Render(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities, Graphics graphics);
+
+    protected abstract ImageOutputConfig CreateConfig(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities);
 }
 #pragma warning restore CA1416 // Validate platform compatibility
---
+++ src/PtahBuilder.BuildSystem/Steps/Output/Imaging/CreateTiledImageStep.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using PtahBuilder.BuildSystem.Config;
+using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+
+#pragma warning disable CA1416 // Validate platform compatibility
+namespace PtahBuilder.BuildSystem.Steps.Output.Imaging;
+
+public record EntityImageConfig(string Filename, int Width, int Height, int Columns, ImageFormat? ImageFormat = null);
+
+public abstract class CreateTiledImageStep<T> : CreateImageStep<T>
+{
+    private EntityImageConfig _entityImageConfig = null!;
+
+    protected CreateTiledImageStep(IFilesConfig filesConfig) : base(filesConfig)
+    {
+    }
+
+    protected sealed override async Task Render(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities, Graphics graphics)
+    {
+        var x = 0;
+        var y = 0;
+
+        foreach (var entity in entities)
+        {
+            var xPos = x * _entityImageConfig.Width;
+            var yPos = y * _entityImageConfig.Height;
+            var region = new Region(new Rectangle(xPos, yPos, _entityImageConfig.Width, _entityImageConfig.Height));
+
+            graphics.Clip = region;
+            graphics.TranslateTransform(xPos, yPos);
+
+            await RenderEntity(context, entity, graphics);
+
+            x++;
+            if (x >= _entityImageConfig.Columns)
+            {
+                x = 0;
+                y++;
+            }
+        }
+
+        graphics.Clip = new Region();
+        graphics.TranslateTransform(0, 0);
+    }
+
+    protected abstract Task RenderEntity(IPipelineContext<T> context, Entity<T> entity, Graphics graphics);
+
+    protected sealed override ImageOutputConfig CreateConfig(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        _entityImageConfig = CreateConfig();
+
+        var rows = (int)Math.Ceiling((decimal)entities.Count / _entityImageConfig.Columns);
+
+        var width = _entityImageConfig.Width * _entityImageConfig.Columns;
+        var height = rows * _entityImageConfig.Height;
+
+        return new(_entityImageConfig.Filename, width, height, _entityImageConfig.ImageFormat);
+    }
+
+    protected abstract EntityImageConfig CreateConfig();
+}
+
+#pragma warning restore CA1416 // Validate platform compatibility
