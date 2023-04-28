fc504c0978114d16f3ea103a182c7b4687f375f4
Andy Longhurst <andy@longhurst.net>
2023-04-28 15:52:43 +0100

    events work

---
+++ src/PtahBuilder.BuildSystem/Steps/Input/SimpleCopyStep.cs
@@ -0,0 +1,47 @@
+﻿using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+using PtahBuilder.Util.Extensions;
+
+namespace PtahBuilder.BuildSystem.Steps.Input;
+
+/// <summary>
+/// Creates a new instance of an object and uses reflection to copy writable property values from the original entity. Values are copied by reference.
+/// The quantityFunc specifies how many instances should be maintained, per entity - a value of 1 will do nothing, a value of 2 will create 1 copy etc.
+/// </summary>
+public class SimpleCopyStep<T> : IStep<T> where T : class, new()
+{
+    private readonly Func<T, int> _quantityFunc;
+
+    public SimpleCopyStep(Func<T, int> quantityFunc)
+    {
+        _quantityFunc = quantityFunc;
+    }
+
+    public Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        var properties = typeof(T).GetProperties().Where(x => x.CanWrite).ToArray();
+
+        foreach (var entity in entities.ToArray())
+        {
+            var quantity = _quantityFunc.Invoke(entity.Value);
+
+            if (quantity > 1)
+            {
+                for (int i = 1; i < quantity; i++)
+                {
+                    var clone = new T();
+
+                    foreach (var property in properties)
+                    {
+                        var value = property.GetValue(entity.Value);
+                        property.SetValue(clone, value);
+                    }
+
+                    context.AddEntity(clone);
+                }
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/PtahBuilder.BuildSystem/Steps/Output/JsonDumpOutputStep.cs
@@ -0,0 +1,30 @@
+﻿using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+using PtahBuilder.BuildSystem.Services.Serialization;
+using PtahBuilder.Util.Services.Logging;
+
+namespace PtahBuilder.BuildSystem.Steps.Output;
+
+public class JsonDumpOutputStep<T> : IStep<T>
+{
+    private readonly IJsonService _jsonService;
+    private readonly ILogger _logger;
+
+    public JsonDumpOutputStep(IJsonService jsonService, ILogger logger)
+    {
+        _jsonService = jsonService;
+        _logger = logger;
+    }
+
+    public Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        foreach (var entity in entities)
+        {
+            var json = _jsonService.Serialize(entity.Value);
+
+            _logger.Info(json);
+        }
+
+        return Task.CompletedTask;
+    }
+}
