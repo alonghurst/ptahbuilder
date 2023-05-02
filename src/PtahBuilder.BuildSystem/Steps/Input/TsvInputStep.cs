d1989599f9091ffa33d76e1ff2e7d39629146c93
Andy Longhurst <andy@longhurst.net>
2023-05-02 16:26:30 +0100

    tsv

---
+++ src/PtahBuilder.BuildSystem/Steps/Input/TsvInputStep.cs
@@ -0,0 +1,21 @@
+﻿using PtahBuilder.BuildSystem.Config;
+using PtahBuilder.BuildSystem.Services.Serialization;
+using PtahBuilder.Util.Services.Logging;
+
+namespace PtahBuilder.BuildSystem.Steps.Input;
+
+public class TsvInputStep<T> : CsvInputStep<T> where T : class
+{
+    public TsvInputStep(IFilesConfig filesConfig, ILogger logger, string fileName, CsvMapping<T> mapping, IDynamicMappingService dynamicMappingService, CsvReadOptions? options = null) : base(filesConfig, logger, fileName, mapping, dynamicMappingService, FixOptions(options))
+    {
+    }
+
+    private static CsvReadOptions FixOptions(CsvReadOptions? options)
+    {
+        options = options ?? new CsvReadOptions();
+
+        options.ColumnSeparator = "\t";
+
+        return options;
+    }
+}
