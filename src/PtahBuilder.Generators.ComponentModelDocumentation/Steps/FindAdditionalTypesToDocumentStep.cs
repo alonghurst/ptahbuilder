6679946ea9865da5e495176677f1f271ac821fe1
Andy Longhurst <andy@longhurst.net>
2023-07-23 12:53:54 +0100

    input types

---
+++ src/PtahBuilder.Generators.ComponentModelDocumentation/Steps/FindAdditionalTypesToDocumentStep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+
+namespace PtahBuilder.Generators.ComponentModelDocumentation.Steps
+{
+    public class FindAdditionalTypesToDocumentStep : IStep<Type>
+    {
+        public Task Execute(IPipelineContext<Type> context, IReadOnlyCollection<Entity<Type>> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
