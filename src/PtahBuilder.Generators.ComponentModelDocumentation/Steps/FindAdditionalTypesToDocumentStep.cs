ec4bdea03c041fa72e352011e2e22e03231b4f93
Andy Longhurst <andy@longhurst.net>
2023-07-23 13:08:49 +0100

    discovery

--- src/PtahBuilder.Generators.ComponentModelDocumentation/Steps/FindAdditionalTypesToDocumentStep.cs
+++ src/PtahBuilder.Generators.ComponentModelDocumentation/Steps/FindAdditionalTypesToDocumentStep.cs
@@ -5,19 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using PtahBuilder.BuildSystem.Entities;
 using PtahBuilder.BuildSystem.Execution.Abstractions;
+using PtahBuilder.Util.Extensions.Reflection;
+using PtahBuilder.Util.Services.Logging;
 
 namespace PtahBuilder.Generators.ComponentModelDocumentation.Steps
 {
     public class FindAdditionalTypesToDocumentStep : IStep<Type>
     {
+        private readonly ILogger _logger;
+
+        public FindAdditionalTypesToDocumentStep(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public Task Execute(IPipelineContext<Type> context, IReadOnlyCollection<Entity<Type>> entities)
         {
             foreach (var entity in entities)
             {
                 var type = entity.Value;
 
-                var relevantProperties = type.get
+                var prefix = (type.Namespace ?? string.Empty).Split('.').FirstOrDefault() ?? "?";
+
+                var relevantProperties = type.GetWritableProperties();
+
+                foreach (var relevantProperty in relevantProperties)
+                {
+                    var relevantType = relevantProperty.PropertyType;
+
+                    if ((relevantType.Namespace ?? string.Empty).StartsWith(prefix))
+                    {
+                        _logger.Info($"Discovered {relevantType.GetTypeName()} on {type.GetTypeName()}");
+
+                        context.AddEntity(relevantType);
+                    }
+                }
             }
+
+            return Task.CompletedTask;
         }
     }
 }
