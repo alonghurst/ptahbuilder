02de42bee2e8a28ae269609d72c86b24d50cf827
Andy Longhurst <andy@longhurst.net>
2023-07-25 15:41:31 +0100

    added yaml serializer

---
+++ src/PtahBuilder.BuildSystem/Steps/Output/YamlOutputStep.cs
@@ -0,0 +1,32 @@
+﻿using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+using PtahBuilder.BuildSystem.Services.Files;
+using PtahBuilder.BuildSystem.Services.Serialization;
+
+namespace PtahBuilder.BuildSystem.Steps.Output;
+
+public class YamlOutputStep<T> : IStep<T>
+{
+    private readonly IYamlService _yamlService;
+    private readonly IOutputFileService _outputFileService;
+
+    public YamlOutputStep(IYamlService yamlService, IOutputFileService outputFileService)
+    {
+        _yamlService = yamlService;
+        _outputFileService = outputFileService;
+    }
+
+    public async Task Execute(IPipelineContext<T> context, IReadOnlyCollection<Entity<T>> entities)
+    {
+        var tasks = entities.Select(x =>
+        {
+            var file = _outputFileService.GetOutputFileForEntity(x, "yaml");
+
+            var json = _yamlService.Serialize(x.Value);
+
+            return File.WriteAllTextAsync(file, json);
+        });
+
+        await Task.WhenAll(tasks);
+    }
+}
