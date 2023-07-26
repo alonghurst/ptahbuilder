d6c02597fa8d9bcedb8725f5fe4dc1bac5392fc3
Andy Longhurst <andy@longhurst.net>
2023-07-26 16:17:19 +0100

    initial inkport

--- src/PtahBuilder.BuildSystem/Extensions/ExecutionConfigExtensions.cs
+++ src/PtahBuilder.BuildSystem/Extensions/ExecutionConfigExtensions.cs
@@ -1,20 +1,6 @@
-﻿using PtahBuilder.BuildSystem.Config;
-using PtahBuilder.BuildSystem.Steps.Output.SimpleText;
-
-namespace PtahBuilder.BuildSystem.Extensions;
+﻿namespace PtahBuilder.BuildSystem.Extensions;
 
 public static class ExecutionConfigExtensions
 {
-    public static ExecutionConfig WriteSimpleTextFiles(this ExecutionConfig config)
-    {
-        config.AddPipelinePhase(phase =>
-        {
-            phase.AddPipeline<SimpleTextOutput>(p =>
-            {
-                p.AddOutputStep<SimpleTextOutputStep>();
-            });
-        });
-
-        return config;
-    }
+    
 }
