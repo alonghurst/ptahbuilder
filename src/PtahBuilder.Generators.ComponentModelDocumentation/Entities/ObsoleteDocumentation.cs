ba25b32dd239af4b3a58713d219817b8eb3e23e6
Andy Longhurst <andy@longhurst.net>
2023-07-31 10:28:15 +0100

    added build support for sets

--- src/PtahBuilder.Generators.ComponentModelDocumentation/Entities/ObsoleteDocumentation.cs
+++ src/PtahBuilder.Generators.ComponentModelDocumentation/Entities/ObsoleteDocumentation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PtahBuilder.Generators.ComponentModelDocumentation.Entities
+﻿namespace PtahBuilder.Generators.ComponentModelDocumentation.Entities
 {
     public record ObsoleteDocumentation(string Description);
 }
