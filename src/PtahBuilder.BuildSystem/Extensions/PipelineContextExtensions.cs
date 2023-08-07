81e21b3f3e8a605c7db5e98f58fb0efa6560804d
Andy Longhurst <andy@longhurst.net>
2023-08-07 09:52:49 +0100

    pipeline extensions

--- src/PtahBuilder.BuildSystem/Extensions/PipelineContextExtensions.cs
+++ src/PtahBuilder.BuildSystem/Extensions/PipelineContextExtensions.cs
@@ -1,17 +1,18 @@
 ﻿using PtahBuilder.BuildSystem.Config;
+using PtahBuilder.BuildSystem.Entities;
 using PtahBuilder.BuildSystem.Execution.Abstractions;
 
 namespace PtahBuilder.BuildSystem.Extensions;
 
 public static class PipelineContextExtensions
 {
-    public static void AddEntityFromFile<T>(this IPipelineContext<T> context, T entity, string filename, Dictionary<string, object>? metadata = null)
+    public static Entity<T> AddEntityFromFile<T>(this IPipelineContext<T> context, T entity, string filename, Dictionary<string, object>? metadata = null)
     {
         metadata ??= new();
 
         metadata.Add(MetadataKeys.SourceFile, filename);
 
-        context.AddEntity(entity, metadata);
+        return context.AddEntity(entity, metadata);
     }
 
     public static void AddEntities<T>(this IPipelineContext<T> context, IEnumerable<T> entities)
