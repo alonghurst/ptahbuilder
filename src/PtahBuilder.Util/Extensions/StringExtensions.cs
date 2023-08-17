a7808aa8aab7f83867a153928d1cf364ed018285
Andy Longhurst <andy@longhurst.net>
2023-08-17 15:36:52 +0100

    split and flatten

--- src/PtahBuilder.Util/Extensions/StringExtensions.cs
+++ src/PtahBuilder.Util/Extensions/StringExtensions.cs
@@ -21,4 +21,16 @@ public static class StringExtensions
 
         return extension;
     }
+
+    /// <summary>
+    /// Splits all members of a string array by a given separator and returns a 1 dimensional array containing all
+    /// non-empty trimmed values
+    /// </summary>
+    public static string[] SplitAndFlatten(this string[] array, string separator = ",")
+    {
+        return array.SelectMany(x => x.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+    }
 }
