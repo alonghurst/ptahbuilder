231325f6cda9fc816556586ba0913f54834993f8
Andy Longhurst <andy@longhurst.net>
2023-08-25 08:46:25 +0100

    new dynamic mapping method

--- src/PtahBuilder.BuildSystem/Extensions/DynamicMappingServiceExtensions.cs
+++ src/PtahBuilder.BuildSystem/Extensions/DynamicMappingServiceExtensions.cs
@@ -6,12 +6,29 @@ namespace PtahBuilder.BuildSystem.Extensions
     {
         public static void MapColumnsToEntity<T>(this IDynamicMappingService service, T entity, IEnumerable<KeyValuePair<string, int>> mappings, string[] columns) where T : class
         {
-            foreach (var mapping  in mappings)
+            foreach (var mapping in mappings)
             {
                 var property = mapping.Key;
                 var value = columns[mapping.Value];
 
                 service.Map(entity, property, value);
+            }
+        }
+
+        public static void MapSeparatedPropertiesToEntity<T>(this IDynamicMappingService service, T entity, string propertyList, char pairSeparator = ',', char valueSeparator = ':') where T : class
+        {
+            var propertyValues = propertyList.Split(propertyList, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var property in propertyValues)
+            {
+                var split = property.Split(valueSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+                if (split.Length != 2)
+                {
+                    throw new InvalidOperationException($"Unable to split \"{property}\" into a property / value pair");
+                }
+
+                service.Map(entity, split[0], split[1]);
             }
         }
     }
