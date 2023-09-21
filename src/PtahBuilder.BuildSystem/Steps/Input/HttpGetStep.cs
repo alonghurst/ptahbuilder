60e812e1601129b0477e5219838be6f40ba368a3
Andy Longhurst <andy@longhurst.net>
2023-09-21 18:20:29 +0100

    resharper tidies

--- src/PtahBuilder.BuildSystem/Steps/Input/HttpGetStep.cs
+++ src/PtahBuilder.BuildSystem/Steps/Input/HttpGetStep.cs
@@ -1,46 +1,45 @@
 ﻿using PtahBuilder.BuildSystem.Entities;
 using PtahBuilder.BuildSystem.Execution.Abstractions;
 
-namespace PtahBuilder.BuildSystem.Steps.Input
+namespace PtahBuilder.BuildSystem.Steps.Input;
+
+public class HttpGetStep : IStep<string>
 {
-    public class HttpGetStep : IStep<string>
+    private readonly string[] _urls;
+
+    public HttpGetStep(string[] urls)
     {
-        private readonly string[] _urls;
+        _urls = urls;
+    }
 
-        public HttpGetStep(string[] urls)
+    public HttpGetStep(string url) : this(new string[] { url })
+    {
+
+    }
+
+    public async Task Execute(IPipelineContext<string> context, IReadOnlyCollection<Entity<string>> entities)
+    {
+        // ReSharper disable once ConvertToUsingDeclaration
+        using (var httpClient = new HttpClient())
         {
-            _urls = urls;
-        }
-
-        public HttpGetStep(string url) : this(new string[] { url })
-        {
-
-        }
-
-        public async Task Execute(IPipelineContext<string> context, IReadOnlyCollection<Entity<string>> entities)
-        {
-            // ReSharper disable once ConvertToUsingDeclaration
-            using (var httpClient = new HttpClient())
+            var tasks = _urls.Select(async url =>
             {
-                var tasks = _urls.Select(async url =>
+                // ReSharper disable once AccessToDisposedClosure
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    // ReSharper disable once AccessToDisposedClosure
-                    var response = await httpClient.GetAsync(url);
+                    var data = await response.Content.ReadAsStringAsync();
 
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var data = await response.Content.ReadAsStringAsync();
+                    context.AddEntityWithId(data, url);
+                }
+                else
+                {
+                    throw new HttpRequestException($"Failed to GET ({response.StatusCode})", null, response.StatusCode);
+                }
+            });
 
-                        context.AddEntityWithId(data, url);
-                    }
-                    else
-                    {
-                        throw new HttpRequestException($"Failed to GET ({response.StatusCode})", null, response.StatusCode);
-                    }
-                });
-
-                await Task.WhenAll(tasks);
-            }
+            await Task.WhenAll(tasks);
         }
     }
 }
