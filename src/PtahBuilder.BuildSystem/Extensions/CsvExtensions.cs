53222ae215f3a3a59637c7ada4ae4d22ce13a2df
Andy Longhurst <andy@longhurst.net>
2023-09-21 18:20:29 +0100

    resharper tidies

--- src/PtahBuilder.BuildSystem/Extensions/CsvExtensions.cs
+++ src/PtahBuilder.BuildSystem/Extensions/CsvExtensions.cs
@@ -1,15 +1,14 @@
-﻿namespace PtahBuilder.BuildSystem.Extensions
-{
-    public static class CsvExtensions
-    {
-        public static int ToColumn(this string value)
-        {
-            return value.ToLower()[0] - 'a';
-        }
+﻿namespace PtahBuilder.BuildSystem.Extensions;
 
-        public static Dictionary<string, int> ToColumns(this IEnumerable<KeyValuePair<string, string>> columns)
-        {
-            return columns.ToDictionary(x => x.Key, x => x.Value.ToColumn());
-        }
+public static class CsvExtensions
+{
+    public static int ToColumn(this string value)
+    {
+        return value.ToLower()[0] - 'a';
+    }
+
+    public static Dictionary<string, int> ToColumns(this IEnumerable<KeyValuePair<string, string>> columns)
+    {
+        return columns.ToDictionary(x => x.Key, x => x.Value.ToColumn());
     }
 }
--- src/PtahBuilder.BuildSystem/Extensions/DynamicMappingServiceExtensions.cs
+++ src/PtahBuilder.BuildSystem/Extensions/DynamicMappingServiceExtensions.cs
@@ -1,40 +1,39 @@
 ﻿using PtahBuilder.BuildSystem.Services.Serialization;
 
-namespace PtahBuilder.BuildSystem.Extensions
-{
-    public static class DynamicMappingServiceExtensions
-    {
-        public static void MapColumnsToEntity<T>(this IDynamicMappingService service, T entity, IEnumerable<KeyValuePair<string, int>> mappings, string[] columns) where T : class
-        {
-            foreach (var mapping in mappings)
-            {
-                var property = mapping.Key;
-                var value = columns[mapping.Value];
+namespace PtahBuilder.BuildSystem.Extensions;
 
-                service.Map(entity, property, value);
-            }
+public static class DynamicMappingServiceExtensions
+{
+    public static void MapColumnsToEntity<T>(this IDynamicMappingService service, T entity, IEnumerable<KeyValuePair<string, int>> mappings, string[] columns) where T : class
+    {
+        foreach (var mapping in mappings)
+        {
+            var property = mapping.Key;
+            var value = columns[mapping.Value];
+
+            service.Map(entity, property, value);
+        }
+    }
+
+    public static void MapSeparatedPropertiesToEntity<T>(this IDynamicMappingService service, T entity, string propertyList, char pairSeparator = ',', char valueSeparator = ':') where T : class
+    {
+        var propertyValues = propertyList.Split(propertyList, StringSplitOptions.RemoveEmptyEntries);
+
+        if (!string.IsNullOrWhiteSpace(propertyList) && propertyValues.Length == 0)
+        {
+            propertyValues = new[] { propertyList };
         }
 
-        public static void MapSeparatedPropertiesToEntity<T>(this IDynamicMappingService service, T entity, string propertyList, char pairSeparator = ',', char valueSeparator = ':') where T : class
+        foreach (var property in propertyValues)
         {
-            var propertyValues = propertyList.Split(propertyList, StringSplitOptions.RemoveEmptyEntries);
+            var split = property.Split(valueSeparator, StringSplitOptions.RemoveEmptyEntries);
 
-            if (!string.IsNullOrWhiteSpace(propertyList) && propertyValues.Length == 0)
+            if (split.Length != 2)
             {
-                propertyValues = new[] { propertyList };
+                throw new InvalidOperationException($"Unable to split \"{property}\" into a property / value pair");
             }
 
-            foreach (var property in propertyValues)
-            {
-                var split = property.Split(valueSeparator, StringSplitOptions.RemoveEmptyEntries);
-
-                if (split.Length != 2)
-                {
-                    throw new InvalidOperationException($"Unable to split \"{property}\" into a property / value pair");
-                }
-
-                service.Map(entity, split[0], split[1]);
-            }
+            service.Map(entity, split[0], split[1]);
         }
     }
 }
