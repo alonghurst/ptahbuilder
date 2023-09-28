dcbe5a6507156afa278f6721a19eea6f641b31c2
Andy Longhurst <andy@longhurst.net>
2023-09-28 16:38:58 +0100

    added toslug extension

--- src/PtahBuilder.Util/Extensions/StringExtensions.cs
+++ src/PtahBuilder.Util/Extensions/StringExtensions.cs
@@ -1,7 +1,11 @@
-﻿namespace PtahBuilder.Util.Extensions;
+﻿using System.Text.RegularExpressions;
+
+namespace PtahBuilder.Util.Extensions;
 
 public static class StringExtensions
 {
+    private static Regex _slugRegex = new("[^a-zA-Z0-9 -]");
+
     public static string ToFileTypeWildCard(this string fileType)
     {
         if (!fileType.StartsWith("*."))
@@ -32,5 +36,19 @@ public static class StringExtensions
             .Select(x => x.Trim())
             .Where(x => !string.IsNullOrWhiteSpace(x))
             .ToArray();
+    }
+
+    public static string ToSlug(this string text)
+    {
+        text = text.ToLower().Replace(" ", "-");
+        
+        text = _slugRegex.Replace(text, "");
+
+        while (text.Contains("--"))
+        {
+            text = text.Replace("--", "-");
+        }
+
+        return text;
     }
 }
