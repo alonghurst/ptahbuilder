09989c8ee2c4ac0237bf4943f660911ffba2a337
Andy Longhurst <andy@longhurst.net>
2023-09-28 11:35:15 +0100

    renamed to google sheet step

---
+++ src/PtahBuilder.BuildSystem/Steps/Output/GoogleSheetStringToTsvFileStep.cs
@@ -0,0 +1,32 @@
+﻿using PtahBuilder.BuildSystem.Config;
+using PtahBuilder.BuildSystem.Entities;
+using PtahBuilder.BuildSystem.Execution.Abstractions;
+
+namespace MarcoYolo.DataGenerator.Steps.DownloadDataSheets;
+
+public class GoogleSheetStringToTsvFileStep : IStep<string>
+{
+    private readonly IFilesConfig _filesConfig;
+
+    public GoogleSheetStringToTsvFileStep(IFilesConfig filesConfig)
+    {
+        _filesConfig = filesConfig;
+    }
+
+    public Task Execute(IPipelineContext<string> context, IReadOnlyCollection<Entity<string>> entities)
+    {
+        var tasks = entities.Select(x =>
+        {
+            var content = x.Value;
+            var url = x.Id;
+
+            var filename = url.Split("sheet=", StringSplitOptions.RemoveEmptyEntries)[1];
+
+            var file = Path.Combine(_filesConfig.DataDirectory, $"{filename}.tsv");
+
+            return File.WriteAllTextAsync(file, content);
+        });
+
+        return Task.WhenAll(tasks);
+    }
+}
