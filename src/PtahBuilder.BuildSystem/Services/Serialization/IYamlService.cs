8184876101d8ca64de2b37366b9b38a710182254
Andy Longhurst <andy@longhurst.net>
2024-03-07 21:12:30 +0000

    refactored yaml tool

--- src/PtahBuilder.BuildSystem/Services/Serialization/IYamlService.cs
+++ src/PtahBuilder.BuildSystem/Services/Serialization/IYamlService.cs
@@ -3,5 +3,6 @@
 public interface IYamlService
 {
     T Deserialize<T>(string yaml);
+    (T entity, Dictionary<string, object>?) DeserializeAndGetMetadata<T>(string yaml);
     string Serialize<T>(T entity);
 }
