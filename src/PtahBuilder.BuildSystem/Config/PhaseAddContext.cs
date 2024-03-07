7ab6e62fc3fa5de0e2a30580667b86b55be547ea
Andy Longhurst <andy@longhurst.net>
2024-03-07 21:36:11 +0000

    setting default id generator

--- src/PtahBuilder.BuildSystem/Config/PhaseAddContext.cs
+++ src/PtahBuilder.BuildSystem/Config/PhaseAddContext.cs
@@ -2,13 +2,25 @@
 
 public class PhaseAddContext
 {
+    public PhaseAddContext(ExecutionConfig executionConfig)
+    {
+        _executionConfig = executionConfig;
+    }
+
     public List<PipelineConfig> EntityPipelines { get; } = new();
+
+    private readonly ExecutionConfig _executionConfig;
 
     public PhaseAddContext AddPipeline<T>(Action<PipelineConfig<T>> configure, string? name = null)
     {
         name = string.IsNullOrWhiteSpace(name) ? $"{typeof(T).Name}_Pipeline" : name;
 
         var pipeline = new PipelineConfig<T>(name);
+
+        if (_executionConfig.DefaultIdGenerator != null)
+        {
+            pipeline.GenerateId = _executionConfig.DefaultIdGenerator;
+        }
 
         configure(pipeline);
 
