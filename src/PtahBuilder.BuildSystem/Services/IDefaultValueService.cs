5054b6564e56068df9c500287f6b003db11aa8ad
Andy Longhurst <andy@longhurst.net>
2024-03-08 09:14:50 +0000

    Added filter build only and default value service

---
+++ src/PtahBuilder.BuildSystem/Services/IDefaultValueService.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace PtahBuilder.BuildSystem.Services;
+
+public interface IDefaultValueService
+{
+    /// <summary>
+    /// Given an input instance returns any properties which have been modified from the default new() of the instance's type
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <returns></returns>
+    IEnumerable<KeyValuePair<PropertyInfo, object>> GetNonDefaultPropertyAndTheNewValue(object instance);
+}
