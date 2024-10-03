7f9b8c24362c5cde4bb3fa1bc395894a389913f1
Andy Longhurst <andy@longhurst.net>
2024-10-03 08:46:14 +0100

    inject json settings

--- src/PtahBuilder.Plugins.NewtonsoftJson/NewtonsoftJsonBuilderExtensions.cs
+++ src/PtahBuilder.Plugins.NewtonsoftJson/NewtonsoftJsonBuilderExtensions.cs
@@ -7,12 +7,67 @@ using PtahBuilder.BuildSystem.Services.Serialization;
 using PtahBuilder.Plugins.NewtonsoftJson.Config.Internal;
 using PtahBuilder.Plugins.NewtonsoftJson.Services;
 using PtahBuilder.Util.Helpers;
+using YamlDotNet.Serialization;
 
 namespace PtahBuilder.Plugins.NewtonsoftJson;
 
 public static class NewtonsoftJsonBuilderExtensions
 {
     public static BuilderFactory UseNewtonsoftJson(this BuilderFactory builderFactory, Assembly jsonConvertersAssembly)
+    {
+        var converters = GetConvertersFromAssembly(jsonConvertersAssembly);
+
+        return builderFactory.UseNewtonsoftJson(converters.ToArray());
+    }
+
+    public static BuilderFactory UseNewtonsoftJson(this BuilderFactory builderFactory, Assembly jsonConvertersAssembly, JsonSerializerSettings settings)
+    {
+        var converters = GetConvertersFromAssembly(jsonConvertersAssembly);
+
+        return builderFactory.UseNewtonsoftJson(settings, converters.ToArray());
+    }
+
+    public static BuilderFactory UseNewtonsoftJson(this BuilderFactory builderFactory,  params JsonConverter[] converters)
+    {
+        var settings = new JsonSerializerSettings()
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+        return builderFactory.UseNewtonsoftJson(settings, converters);
+    }
+
+    public static BuilderFactory UseNewtonsoftJson(this BuilderFactory builderFactory, JsonSerializerSettings settings, params JsonConverter[] converters)
+    {
+        builderFactory.ConfigureServices(services =>
+        {
+            services.AddSingleton(new NewtonsoftJsonConverterConfig(converters));
+
+            services.AddSingleton(settings);
+
+            var descriptor = new ServiceDescriptor(typeof(IJsonService), typeof(NewtonsoftJsonService), ServiceLifetime.Singleton);
+
+            services.Replace(descriptor);
+        });
+
+        return builderFactory;
+    }
+
+    public static BuilderFactory ReplaceNewtonsoftJsonSettings(this BuilderFactory builderFactory, JsonSerializerSettings settings)
+    {
+        builderFactory.ConfigureServices(services =>
+        {
+            var descriptor = new ServiceDescriptor(typeof(JsonSerializerSettings), settings);
+
+            services.Replace(descriptor);
+        });
+
+        return builderFactory;
+    }
+
+    private static List<JsonConverter> GetConvertersFromAssembly(Assembly jsonConvertersAssembly)
     {
         var types = ReflectionHelper.GetLoadedTypesThatAreAssignableTo(typeof(JsonConverter), assemblyFilter: jsonConvertersAssembly.FullName!);
 
@@ -26,20 +81,7 @@ public static class NewtonsoftJsonBuilderExtensions
             }
         }
 
-        return builderFactory.UseNewtonsoftJson(converters.ToArray());
+        return converters;
     }
 
-    public static BuilderFactory UseNewtonsoftJson(this BuilderFactory builderFactory, params JsonConverter[] converters)
-    {
-        builderFactory.ConfigureServices(services =>
-        {
-            services.AddSingleton(new NewtonsoftJsonConverterConfig(converters));
-
-            var descriptor = new ServiceDescriptor(typeof(IJsonService), typeof(NewtonsoftJsonService), ServiceLifetime.Singleton);
-
-            services.Replace(descriptor);
-        });
-
-        return builderFactory;
-    }
 }
