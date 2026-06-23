e7f42687bf41626c144b917e8ee1166cfd1a6cc3
Andy Longhurst <andy@longhurst.net>
2026-06-23 14:03:22 +0100

    added new data

--- src/PtahBuilder.BuildSystem/Services/Serialization/YamlService.cs
+++ src/PtahBuilder.BuildSystem/Services/Serialization/YamlService.cs
@@ -4,6 +4,7 @@ using PtahBuilder.Util.Extensions.Reflection;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 using PtahBuilder.BuildSystem.Services.Mapping;
+using PtahBuilder.Util.Helpers;
 
 namespace PtahBuilder.BuildSystem.Services.Serialization;
 
